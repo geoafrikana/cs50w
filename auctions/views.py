106b9d4de253ff007c90bbee6c1beed287dbcce7
GeoAfrikana <geoafrikana@gmail.com>
2020-12-09 22:01:46 +0100

    copied form from my project 1 into create listing view

--- auctions/views.py
+++ auctions/views.py
@@ -61,3 +61,10 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_listing(request):
+    if request.method == 'POST':
+        return render(request, "auctions/create_listing.html", {"message" : 'form filled'})
+        
+    return render(request, "auctions/create_listing.html", {"message" : 'fill the form'})
